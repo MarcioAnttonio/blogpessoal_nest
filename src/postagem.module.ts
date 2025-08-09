f54ef07cb711ae360b9933654ff33b448f0df1df
Marcio Barbeiro <marciobarbeiro85@gmail.com>
2025-08-09 01:01:12 -0300

    correção postagem.module.ts, que estava fora da pasta correta

---
+++ src/postagem.module.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from "@nestjs/common";
+import { Postagem } from "./postagem/entities/postagem.entity";
+
+
+@Module({
+    imports:[TypeOrmModule.forFeature([Postagem])],
+    controllers: [],
+    providers: [],
+    exports: [],
+
+})
+export class PostagemModule {}
